15039f6d2a075a602398d665eb18f940c8ab948a
alanensastegui <alan.ensastegui@gmail.com>
2025-08-22 21:05:54 -0700

    preview / dev price ids

--- lib/subscriptions/price-ids.ts
+++ lib/subscriptions/price-ids.ts
@@ -7,25 +7,25 @@ export const PRICE_IDS: Record<RuntimeEnvironment, {
   professional_plus: { monthly: string; annual: string };
 }> = {
   development: {
-    free_trial: "price_1RWMgmLBwjY0mWjvWBn3bbaM",
+    free_trial: "price_1RWNJsHSlLIGGSTujoK9WPEG",
     professional: {
-      monthly: "price_1RWMgmLBwjY0mWjvWBn3bbaM",
-      annual: "price_1Rlg1ALBwjY0mWjvoASzYgKk",
+      monthly: "price_1RWNJsHSlLIGGSTujoK9WPEG",
+      annual: "price_1RlgdNHSlLIGGSTubGcGyGSn",
     },
     professional_plus: {
-      monthly: "price_1RWMinLBwjY0mWjvNsqW5b0B",
-      annual: "price_1Rlg1eLBwjY0mWjv0ju095LY",
+      monthly: "price_1RWNMtHSlLIGGSTuYFXrQJ07",
+      annual: "price_1RlgdCHSlLIGGSTuZLTTFR1c",
     },
   },
   preview: {
-    free_trial: "price_1RWMgmLBwjY0mWjvWBn3bbaM",
+    free_trial: "price_1RWMl9LBwjY0mWjvMzEzck16",
     professional: {
-      monthly: "price_1RWMgmLBwjY0mWjvWBn3bbaM",
-      annual: "price_1Rlg1ALBwjY0mWjvoASzYgKk",
+      monthly: "price_1RWMl9LBwjY0mWjvMzEzck16",
+      annual: "price_1RlgcGLBwjY0mWjv7HoHGUwc",
     },
     professional_plus: {
-      monthly: "price_1RWMinLBwjY0mWjvNsqW5b0B",
-      annual: "price_1Rlg1eLBwjY0mWjv0ju095LY",
+      monthly: "price_1RWMm0LBwjY0mWjv2RPAqIuk",
+      annual: "price_1RlgbnLBwjY0mWjvFfWunbRd",
     },
   },
   prod: {
